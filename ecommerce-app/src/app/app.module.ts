217807ab0c1fd6da05b019137016c5f1ad334031
samandeep <amandeep.singh@deakin.edu.au>
2020-12-05 13:42:18 +1100

    Adding Products module & component

--- ecommerce-app/src/app/app.module.ts
+++ ecommerce-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { HomeComponent } from './home/home.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, 
-    MaterialModule
+    MaterialModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
