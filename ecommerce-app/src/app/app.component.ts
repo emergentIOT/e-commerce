9e97d80658d5c3a420b2c2d5595cbb2e1ff72939
samandeep <amandeep.singh@deakin.edu.au>
2020-12-10 12:59:27 +1100

    Update login nav-link

--- ecommerce-app/src/app/app.component.ts
+++ ecommerce-app/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
+import { User } from './interfaces/User';
+import { Subscribable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+
+
   title = 'ecommerce-app';
+
+  user: User | undefined;
+  userSubscription: Subscription | undefined;
+
+  ngOnInit(): void {
+   
+  }
+
+  ngOnDestroy(): void {
+    if(this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
+
+  
+
+  constructor(private authService: AuthService,
+    private router: Router) {
+     this.userSubscription = this.authService.user.subscribe(user => {
+      console.log("this.user", this.user);    
+      this.user = user;
+     });
+
+     console.log("this.userSsubscription", this.userSubscription);
+  }
+
+  logout() {
+      this.authService.logout();
+      this.router.navigate(['/']);
+  }
+
+
+
+
 }
--- ecommerce-app/src/app/auth/auth.service.ts
+++ ecommerce-app/src/app/auth/auth.service.ts
@@ -18,14 +18,27 @@ export class AuthService {
     return of(loginCredentials);
   }
 
+  logout() {
+    //Remove user from Subject
+    //Clean tokens
+    //this.user$.next(null);
+    console.log("User has been logout");
+  }
+
   get user() {
     return this.user$.asObservable();
   }
 
-  register(user: User) {
-    const registerCredentials = { user };
+  register(user: any) {
+    
+    //Make an API call to save user in db.
+    //update the user subject.
+    this.setUser(user);
+    console.log("User Regitration", user);
+    return of(user);
+  }
+
+  private setUser(user: User | undefined) {
     this.user$.next(user);
-    console.log("User Regitration", registerCredentials);
-    return of(registerCredentials);
   }
 }
