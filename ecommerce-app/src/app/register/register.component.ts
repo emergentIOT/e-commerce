0736677d2ddf6e8abfbe55ac25b3b2e661807e93
samandeep <amandeep.singh@deakin.edu.au>
2020-12-15 14:11:18 +1100

    Update register form validation

--- ecommerce-app/src/app/register/register.component.ts
+++ ecommerce-app/src/app/register/register.component.ts
@@ -15,6 +15,7 @@ export class RegisterComponent implements OnInit {
     name : new FormControl('', [Validators.required]),
     email : new FormControl('', [Validators.required, Validators.email]),
     password : new FormControl('', [Validators.required]),
+    repeatPassword: new FormControl('', [Validators.required, this.passwordMatch])
   });
   constructor(private  authService: AuthService,
     private router: Router) { }
@@ -23,13 +24,43 @@ export class RegisterComponent implements OnInit {
      
   }
 
+  //Custom password match
+  passwordMatch(control: FormControl) {
+    let password = control.root.get('password');
+    return password && control.value !== password.value ? {
+      passwordMatch: true
+    } : null ;
+  }
+
   register() {
+    if(!this.userGroup.valid) {
+      return;
+    }
+
     const user = this.userGroup.getRawValue();
     this.authService.register(user).subscribe(data => {
       console.log("Data received");
       this.router.navigate(['/login']);
     })
   }
+
+  get fullname() {
+    return this.userGroup.get('fullname');
+  }
+
+  get email() {
+    return this.userGroup.get('email');
+  }
+
+  get password() {
+    return this.userGroup.get('password');
+  }
+
+  get repeatPassword() {
+    return this.userGroup.get('repeatPassword');
+  }
+
+
 
 
 
